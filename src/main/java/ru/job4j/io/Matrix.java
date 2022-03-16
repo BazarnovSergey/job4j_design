4039d53e491dd8901e9f854f4372e69febcc8273
macbookpro <101sergey101@gmail.com>
2022-03-16 20:03:11 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.util.Arrays;
+
+public class Matrix {
+    public static int[][] multiple(int size) {
+        int[][] data = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                data[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return data;
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write(Arrays.deepToString(multiple(10)).getBytes());
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
