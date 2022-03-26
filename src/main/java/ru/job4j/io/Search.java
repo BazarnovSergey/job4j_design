f52bf634b9a135beeff8a43ac2deb6da08795142
macbookpro <101sergey101@gmail.com>
2022-03-26 19:35:29 +0300

    5. Валидация параметров запуска. [#246865 #280685]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,18 +11,33 @@ import java.util.function.Predicate;
 public class Search {
     public static void main(String[] args) throws IOException {
 
-        if (args.length < 2) {
+        if (args.length != 2) {
             throw new IllegalArgumentException(
                     "You must specify the root folder and file extension."
                             + " Usage java -jar dir.jar ROOT_FOLDER FILE_EXTENSION.");
         }
-        Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+
+        if (checkValidateParametersArgs(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static boolean checkValidateParametersArgs(String[] args) {
+        if (!new File(args[0]).isDirectory()) {
+            throw new IllegalArgumentException("First argument must be a directory");
+        }
+
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException(
+                    "The second argument must start with character \".\"");
+        }
+        return true;
     }
 }
