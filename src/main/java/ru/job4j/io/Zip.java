4942267f06a7af154ebc797b4b153916ce0be0f8
Bazarnov Sergey <ssergey101@yandex.ru>
2022-03-27 16:45:06 +0300

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public static void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(
+                        new FileInputStream(source.toFile()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Enter all arguments. Use pattern:"
+                    + "-d=c:\\project\\job4j\\ -e=.class -o=project.zip\n"
+                    + "-d - directory - which we want to archive.\n"
+                    + "-e - exclude - exclude files with the extension class.\n"
+                    + "-o - output - what we archive into");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        Path start = Paths.get(argsName.get("d"));
+        String exclude = argsName.get("e");
+        File output = new File(argsName.get("o"));
+        List<Path> filesWithoutExcluded = new ArrayList<>();
+        try {
+            filesWithoutExcluded = Search.search(start, p -> !p.toFile().getName().endsWith(exclude));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(filesWithoutExcluded);
+        packFiles(filesWithoutExcluded, output);
+    }
+}
