d9bf744075fc4fd7bc596b79ca77364635c3d4e5
BazarnovSergey <ssergey101@yandex.ru>
2022-04-23 23:01:13 +0300

    0. JDBC [#6863 #292501]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -13,7 +13,7 @@ public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Config config = new Config(ClassLoader.getSystemResource("app.properties").getPath());
         config.load();
-        Class.forName("org.postgresql.Driver");
+        Class.forName(config.value("driver"));
         String url = config.value("url");
         String login = config.value("login");
         String password = config.value("password");
