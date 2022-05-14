44f53619930d93063f5f079bd0bf382d4a74ce62
BazarnovSergey <ssergey101@yandex.ru>
2022-05-14 21:00:12 +0300

    1. Демонстрация работы GC [#1589]

---
+++ src/main/java/ru/job4j/gc/User.java
@@ -0,0 +1,41 @@
+package ru.job4j.gc;
+
+public class User {
+    private int id;
+    private String name;
+
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.printf("Removed %d %s%n", id, name);
+    }
+
+    public static void main(String[] args) {
+        GCDemo.info();
+        for (int i = 1; i < 18200; i++) {
+            new User(i, "Name" + i);
+        }
+        GCDemo.info();
+    }
+}
